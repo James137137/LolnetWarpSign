60114ac9c55a76565e36edbd195be5bb1be26849
James Anderson <james137137@lolnet.co.nz>
2013-04-30 08:57:30 +1200

    version: 0.22

--- src/com/james137137/LolnetWarpSign/LolnetWarpSignListener.java
+++ src/com/james137137/LolnetWarpSign/LolnetWarpSignListener.java
@@ -41,18 +41,14 @@ class LolnetWarpSignListener implements Listener {
         this.LolnetWarpSign = aThis;
         this.economy = economy;
         config = aThis.getConfig();
-        if (aThis.getServer().getPluginManager().getPlugin("GriefPrevention") != null)
-        {
+        if (aThis.getServer().getPluginManager().getPlugin("GriefPrevention") != null) {
             griefPreventionEnable = true;
         }
     }
 
     @EventHandler
-    public void onPlayerBreakSign (BlockBreakEvent event)
-    {
-        
-        if (event.isCancelled() || event.getPlayer().hasPermission("LolnetWarpSign.BreakWarp.bypass"))
-        {
+    public void onPlayerBreakSign(BlockBreakEvent event) {
+        if (event.isCancelled() || event.getPlayer().hasPermission("LolnetWarpSign.BreakWarp.bypass")) {
             return;
         }
         Player player = event.getPlayer();
@@ -61,82 +57,95 @@ class LolnetWarpSignListener implements Listener {
         if ((mat == Material.SIGN_POST.getId()) || (mat == Material.WALL_SIGN.getId())) {
             Sign lolnetSign = (Sign) block.getState();
             String Line3 = lolnetSign.getLine(3);
-            if (Line3.length()<=2)
-            {
+            if (Line3.length() <= 2) {
                 return;
             }
-            if (!Line3.equalsIgnoreCase(ChatColor.GREEN +player.getName()) && Line3.substring(0, 2).equals(""+ChatColor.GREEN))
-            {
+            if (!Line3.equalsIgnoreCase(ChatColor.GREEN + player.getName()) && Line3.substring(0, 2).equals("" + ChatColor.GREEN)) {
                 event.setCancelled(true);
             }
         }
     }
+
     @EventHandler
     public void onPlayerCommand(PlayerCommandPreprocessEvent event) {
-        Player player = event.getPlayer();
-        if (event.isCancelled() || player.hasPermission("LolnetWarpSign.SetWarp.bypass") || !player.hasPermission("essentials.setwarp")) {
-            return;
-        }
-
-        String message = event.getMessage();
-        
-        String Command = "";
-        String warpName = "[none]";
-        for (int i = 1; i < message.length(); i++) {
-            if (message.charAt(i) == ' ') {
-                warpName = message.substring(i + 1, message.length());
-                break;
-            }
-            Command += message.charAt(i);
-        }
-        
-        if (Command.equalsIgnoreCase("setwarp")) {
-            if (warpName.contains(" "))
-            {
-                player.sendMessage(ChatColor.RED + "Invaild warp name. Please make sure it doesn't contain spaces");
-                event.setCancelled(true);
+        if (event.getMessage().contains("setwarp") || event.getMessage().contains("createwarp")) {
+            Player player = event.getPlayer();
+            if (event.isCancelled() || player.hasPermission("LolnetWarpSign.SetWarp.bypass") || !player.hasPermission("essentials.setwarp")) {
                 return;
             }
-            if (griefPreventionEnable) {
-                Claim claim = GriefPrevention.instance.dataStore.getClaimAt(player.getLocation(), true, null);
-                if (claim == null) {
-                    player.sendMessage(ChatColor.RED +"You must be in a claim to /setwarp");
-                    event.setCancelled(true);
-                    return;
-                } else if (claim.allowBuild(player) != null) {
-                    player.sendMessage(ChatColor.RED +"You must have permission to build in this claim to /setwarp");
+
+            String message = event.getMessage();
+            if (message.contains("createwarp"))
+            {
+                 player.sendMessage(ChatColor.RED + "please use /setwarp");
+                 event.setCancelled(true);
+                 return;
+            }
+
+            String Command = "";
+            String warpName = "";
+            for (int i = 1; i < message.length(); i++) {
+                if (message.charAt(i) == ' ') {
+                    warpName = message.substring(i + 1, message.length());
+                    break;
+                }
+                Command += message.charAt(i);
+            }
+
+            if (Command.equalsIgnoreCase("setwarp")) {
+                
+                if (warpName.length() < 3) {
+                    player.sendMessage(ChatColor.RED + "Invaild warp name. it must have a minium of 3 charaters");
                     event.setCancelled(true);
                     return;
                 }
-            }
-            
-            
-            
-            
-            if (economy.getBalance(player.getName()) >= config.getInt("NewWarpCost")) {
-                try {
-                    BufferedReader in = new BufferedReader(new FileReader("plugins/Essentials/warps/" + warpName + ".yml"));
-                    player.sendMessage(ChatColor.DARK_RED + "Warp already exist");
+                if (warpName.contains(" ")) {
+                    player.sendMessage(ChatColor.RED + "Invaild warp name. Please make sure it doesn't contain spaces");
                     event.setCancelled(true);
                     return;
-                } catch (FileNotFoundException ex) {
-                    if (warpName.length() > 15) {
-                        player.sendMessage(ChatColor.RED + "Warp Name is too long please use a shorter name");
+                }
+                if (griefPreventionEnable) {
+                    Claim claim = GriefPrevention.instance.dataStore.getClaimAt(player.getLocation(), true, null);
+                    if (claim == null) {
+                        player.sendMessage(ChatColor.RED + "You must be in a claim to /setwarp");
+                        event.setCancelled(true);
+                        return;
+                    } else if (claim.allowBuild(player) != null) {
+                        player.sendMessage(ChatColor.RED + "You must have permission to build in this claim to /setwarp");
                         event.setCancelled(true);
                         return;
                     }
-                    log.log(Level.INFO, "{0} has set a new warp called: {1} at location: {2},{3},{4}",
-                            new Object[]{player.getName(), warpName, player.getLocation().getBlockX(),
-                                player.getLocation().getBlockY(), player.getLocation().getBlockZ()});
-                    economy.withdrawPlayer(player.getName(), config.getInt("NewWarpCost"));
-                    player.sendMessage(ChatColor.GREEN + "$" + config.getInt("NewWarpCost") + " has been taken off you.");
                 }
-            } else {
-                    player.sendMessage(ChatColor.RED + "you need $" +config.getInt("NewWarpCost"));
+
+
+
+
+                if (economy.getBalance(player.getName()) >= config.getInt("NewWarpCost")) {
+                    try {
+                        BufferedReader in = new BufferedReader(new FileReader("plugins/Essentials/warps/" + warpName + ".yml"));
+                        player.sendMessage(ChatColor.DARK_RED + "Warp already exist");
+                        event.setCancelled(true);
+                        return;
+                    } catch (FileNotFoundException ex) {
+                        if (warpName.length() > 15) {
+                            player.sendMessage(ChatColor.RED + "Warp Name is too long please use a shorter name");
+                            event.setCancelled(true);
+                            return;
+                        }
+                        log.log(Level.INFO, "{0} has set a new warp called: {1} at location: {2},{3},{4}",
+                                new Object[]{player.getName(), warpName, player.getLocation().getBlockX(),
+                            player.getLocation().getBlockY(), player.getLocation().getBlockZ()});
+                        economy.withdrawPlayer(player.getName(), config.getInt("NewWarpCost"));
+                        player.sendMessage(ChatColor.GREEN + "$" + config.getInt("NewWarpCost") + " has been taken off you.");
+                    }
+                } else {
+                    player.sendMessage(ChatColor.RED + "you need $" + config.getInt("NewWarpCost"));
                     event.setCancelled(true);
                     return;
                 }
+            }
         }
+
 
     }
 
@@ -147,65 +156,62 @@ class LolnetWarpSignListener implements Listener {
             return;
         }
         Player player = event.getPlayer();
-        
+
         Block block = event.getClickedBlock();
         int mat = block.getTypeId();
         if ((mat == Material.SIGN_POST.getId()) || (mat == Material.WALL_SIGN.getId())) {
             Sign lolnetSign = (Sign) block.getState();
             if ((lolnetSign.getLine(0).equalsIgnoreCase(ChatColor.DARK_BLUE + "[MyWarp]") || lolnetSign.getLine(0).equalsIgnoreCase(ChatColor.DARK_BLUE + "[PrivateWarp]")) && player.hasPermission("essentials.signs.use.warp")) {
-                if (lolnetSign.getLine(0).equalsIgnoreCase(ChatColor.DARK_BLUE + "[MyWarp]"))
-                {
-                   LolnetWarpSign.TeleportPlayer(player, lolnetSign.getLine(1).toLowerCase(), mat); 
-                } else
-                {
+                if (lolnetSign.getLine(0).equalsIgnoreCase(ChatColor.DARK_BLUE + "[MyWarp]")) {
+                    LolnetWarpSign.TeleportPlayer(player, lolnetSign.getLine(1).toLowerCase(), mat);
+                } else {
                     String Line3 = lolnetSign.getLine(3);
-                    if (Line3.equalsIgnoreCase(ChatColor.GREEN +player.getName()) || player.hasPermission("LolnetWarpSign.WarpSign.bypass"))
-                    {
-                        LolnetWarpSign.TeleportPlayer(player, lolnetSign.getLine(1).toLowerCase(), mat); 
-                    }
-                    else
-                    {
+                    if (Line3.equalsIgnoreCase(ChatColor.GREEN + player.getName()) || player.hasPermission("LolnetWarpSign.WarpSign.bypass")) {
+                        LolnetWarpSign.TeleportPlayer(player, lolnetSign.getLine(1).toLowerCase(), mat);
+                    } else {
                         player.sendMessage(ChatColor.RED + "This is not your Warp Sign");
                     }
                 }
-                
+
                 return;
             }
 
 
-            if ((lolnetSign.getLine(0).equalsIgnoreCase("[myWarp]") || lolnetSign.getLine(0).equalsIgnoreCase("[Warp]") 
-                    || lolnetSign.getLine(0).equalsIgnoreCase("[PrivateWarp]")) 
+            if ((lolnetSign.getLine(0).equalsIgnoreCase("[myWarp]") || lolnetSign.getLine(0).equalsIgnoreCase("[Warp]")
+                    || lolnetSign.getLine(0).equalsIgnoreCase("[PrivateWarp]"))
                     && player.hasPermission("LolnetWarpSign.activate")) {
-                
+
                 if (griefPreventionEnable && !player.hasPermission("LolnetWarpSign.WarpSign.bypass")) {
-                Claim claim = GriefPrevention.instance.dataStore.getClaimAt(lolnetSign.getLocation(), true, null);
-                if (claim == null) {
-                    player.sendMessage(ChatColor.RED +"that sign must be in a claim to activate that sign");
-                    event.setCancelled(true);
-                    return;
-                } else if (claim.allowBuild(player) != null) {
-                    player.sendMessage(ChatColor.RED +"You must have permission to build in this claim to activate that sign");
-                    event.setCancelled(true);
-                    return;
+                    Claim claim = GriefPrevention.instance.dataStore.getClaimAt(lolnetSign.getLocation(), true, null);
+                    if (claim == null) {
+                        player.sendMessage(ChatColor.RED + "that sign must be in a claim to activate that sign");
+                        event.setCancelled(true);
+                        return;
+                    } else if (claim.allowBuild(player) != null) {
+                        player.sendMessage(ChatColor.RED + "You must have permission to build in this claim to activate that sign");
+                        event.setCancelled(true);
+                        return;
+                    }
                 }
-            }
-                
-                
+
+
                 if (economy.getBalance(player.getName()) >= config.getInt("WarpSignCost")) {
                     try {
                         BufferedReader in = new BufferedReader(new FileReader("plugins/Essentials/warps/" + lolnetSign.getLine(1).toLowerCase() + ".yml"));
-                        
-                        if (lolnetSign.getLine(0).equalsIgnoreCase("[PrivateWarp]"))
-                        {
+                        String WarpName = lolnetSign.getLine(1).toLowerCase();
+                        if (WarpName.length() < 1) {
+                            player.sendMessage(ChatColor.DARK_RED + "Warp does not exist");
+                            return;
+                        }
+                        if (lolnetSign.getLine(0).equalsIgnoreCase("[PrivateWarp]")) {
                             lolnetSign.setLine(0, ChatColor.DARK_BLUE + "[PrivateWarp]");
-                        } else
-                        {
+                        } else {
                             lolnetSign.setLine(0, ChatColor.DARK_BLUE + "[MyWarp]");
                         }
                         lolnetSign.setLine(3, ChatColor.GREEN + player.getName());
                         lolnetSign.update();
                         if (!player.hasPermission("LolnetWarpSign.WarpSign.bypass")) {
-                            
+
                             economy.withdrawPlayer(player.getName(), config.getInt("WarpSignCost"));
                             player.sendMessage(ChatColor.GREEN + "$" + config.getInt("WarpSignCost") + " has been taken off you.");
                         }
@@ -214,7 +220,7 @@ class LolnetWarpSignListener implements Listener {
                         player.sendMessage(ChatColor.DARK_RED + "Warp does not exist");
                     }
                 } else {
-                    player.sendMessage(ChatColor.RED + "you need $" +config.getInt("WarpSignCost"));
+                    player.sendMessage(ChatColor.RED + "you need $" + config.getInt("WarpSignCost"));
                 }
 
 
